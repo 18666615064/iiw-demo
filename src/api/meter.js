5dba4856a24588f74cdda2533c4c5a4c687d8e6b
陈贤杰 <644596713@qq.com>
2018-09-24 18:09:43 +0800

    test

--- src/api/meter.js
+++ src/api/meter.js
@@ -1,24 +1,21 @@
 import request from '../utils/request'
-
 export function getInfoImei(url) {
   return request({
     url: `/device/info/imei/${url}`,
     method: 'get'
   })
 }
-
 export function getInfoList(query) {
   return request({
-    url: '/devicecenter/sensor/top',
+    url: '/sensor/top',
     // url: '/sensor/top',
     method: 'get',
     params: query
   })
 }
-
 export function getMInfoList(query) {
   return request({
-    url: '/devicecenter/sensor/last',
+    url: '/sensor/last',
     // url: '/sensor/last',
     method: 'get',
     params: query
